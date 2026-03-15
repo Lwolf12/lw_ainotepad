eddc55c5a73c685754686040e7d1cd3015dc8ce4
Larry <lwolf123@gmail.com>
2026-03-14 22:44:24 -0700

    2026-03-14 22:44

---
+++ _git_push_tmp.bat
@@ -0,0 +1,3 @@
+@echo off
+python "c:\Users\lwolf\Desktop\projects\project_launcher\actions\git_backup\_run_git_push.py" "C:\Users\lwolf\Desktop\projects\ainotepad" "2026-03-14 22:44" 1
+del "%~f0"
